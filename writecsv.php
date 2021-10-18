0c8f170586a0980f7217750774c5a189477106e9
jamesmarchment <james.marchment@gmail.com>
2021-10-18 17:44:39 -0400

    cleaned up commenting

--- writecsv.php
+++ writecsv.php
@@ -1,26 +1,28 @@
 <?php
 
 $file = "log.csv";
+
+// check if there is an existing log file
 if(is_file($file)){
-// file exists
+	// file exists
 $csv = fopen($file, "a");
 }else { 
-// file does not exist, create file
+	// file does not exist, create file
 $csv =fopen($file, "a");
-
-// create headers on first line to identify columns
-$columnNames = array();
-foreach($_POST as $key=>$value){
-$columnNames[] = $key;
+	// create headers on first line to identify columns
+	$columnNames = array();
+	// make array of keys
+	foreach($_POST as $key=>$value){
+		$columnNames[] = $key;
+			}
+	// write as first line of CSV
+	fputcsv($csv, $columnNames);
 }
-fputcsv($csv, $columnNames);
-}
 
+// write submitted data to CSV
 fputcsv($csv, $_POST);
+
 fclose($csv);
-
 var_dump($_POST);
-
-
 
 ?>
